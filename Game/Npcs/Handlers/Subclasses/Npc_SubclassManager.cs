d37d42408a96e14796f8c6c2d0cc1fd198167b25
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2025-12-13 03:43:21 +0200

    refact: Simplify dialog handling in Npc_SubclassManager

--- Game/Npcs/Handlers/Subclasses/Npc_SubclassManager.cs
+++ Game/Npcs/Handlers/Subclasses/Npc_SubclassManager.cs
@@ -14,24 +14,18 @@ namespace MTA.Game.Npcs.Handlers.Subclasses
             switch (npcRequest.OptionID)
             {
                 case 0:
-
-                    switch (npcRequest.OptionID)
                     {
-                        case 0:
-                            {
-                                dialog.Text("Helper classes grant you extra attributes. There are six auxiliary classes: Master of Antidotes, Anticritical Master, Master of Witchcraft, Master of Magic, and Dancer. A character can learn all of them, and each assistant provides different effects. What would you like to do?");
-                                dialog.Option("Enter the hall.", 1);
-                                dialog.Option("See you later.", 255);
-                                dialog.Send();
-                                break;
-                            }
-                        case 1:
-                            {
-                                client.Entity.Teleport(1597, 51, 70);
-                                break;
-                            }
+                        dialog.Text("Helper classes grant you extra attributes. There are six auxiliary classes: Master of Antidotes, Anticritical Master, Master of Witchcraft, Master of Magic, and Dancer. A character can learn all of them, and each assistant provides different effects. What would you like to do?");
+                        dialog.Option("Enter the hall.", 1);
+                        dialog.Option("See you later.", 255);
+                        dialog.Send();
+                        break;
                     }
-                    break;
+                case 1:
+                    {
+                        client.Entity.Teleport(1597, 48, 53);
+                        break;
+                    }
             }
         }
     }
