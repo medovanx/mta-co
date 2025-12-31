2eb4aeb2711d6e61364332c9409bafa00344f382
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2025-12-31 09:05:15 +0200

    refact: streamline NameChange class by improving variable naming, reducing redundancy, and enhancing readability in the UpdateNames method

--- Database/NameChange.cs
+++ Database/NameChange.cs
@@ -1,56 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace MTA.Database {
     public class NameChange {
         public static void UpdateNames() {
-            Dictionary<String, NameChangeC> UPDATE = new Dictionary<string, NameChangeC>();
+            var update = new Dictionary<string, NameChangeC>();
             using (var cmd = new MySqlCommand(MySqlCommandType.SELECT).Select("entities"))
             using (var r = new MySqlReader(cmd)) {
-                String newname = "", name = "";
-                UInt32 uid = 0;
                 while (r.Read()) {
-                    newname = r.ReadString("namechange"); //debug make
-                    name = r.ReadString("name");
-                    if (newname != "" && newname != " ") {
-                        uid = r.ReadUInt32("uid");
+                    var newName = r.ReadString("namechange");
+                    var name = r.ReadString("name");
+                    if (newName is "" or " ") continue;
+                    var uid = r.ReadUInt32("uid");
 
-                        MySqlCommand cmdupdate = null; //lol i see the problem hold on ,,, hold on what? :$ try now
-                        cmdupdate = new MySqlCommand(MySqlCommandType.UPDATE);
-                        cmdupdate.Update("guilds").Set("Name", newname).Where("Name", name).Execute();
+                    var sqlCommand = new MySqlCommand(MySqlCommandType.UPDATE);
+                    sqlCommand.Update("guilds").Set("Name", newName).Where("Name", name).Execute();
 
-                        // LeaderName is now computed from entities table via LeaderID, so no update needed
-                        // LeaderID points to UID which doesn't change, and LeaderName getter will get new name automatically
+                    // LeaderName is now computed from entities table via LeaderID, so no update needed
+                    // LeaderID points to UID which doesn't change, and LeaderName getter will get new name automatically
+                    sqlCommand = new MySqlCommand(MySqlCommandType.UPDATE);
+                    sqlCommand.Update("partners").Set("PartnerName", newName).Where("PartnerID", uid).Execute();
 
-                        cmdupdate = new MySqlCommand(MySqlCommandType.UPDATE);
-                        cmdupdate.Update("partners").Set("PartnerName", newname).Where("PartnerID", uid).Execute();
+                    sqlCommand = new MySqlCommand(MySqlCommandType.UPDATE);
+                    sqlCommand.Update("teamarena").Set("EntityName", newName).Where("EntityID", uid).Execute();
 
-                        cmdupdate = new MySqlCommand(MySqlCommandType.UPDATE);
-                        cmdupdate.Update("teamarena").Set("EntityName", newname).Where("EntityID", uid).Execute();
-
-                        if (!UPDATE.ContainsKey(name))
-                            UPDATE.Add(name, new NameChangeC() { NewName = newname, OldName = name });
-                    }
+                    if (!update.ContainsKey(name))
+                        update.Add(name, new NameChangeC() { NewName = newName, OldName = name });
                 }
             }
 
-            if (UPDATE.Count > 0) {
+            if (update.Count > 0) {
                 Console.WriteLine(" [NAME CHANGES]");
             }
 
-            foreach (NameChangeC names in UPDATE.Values) {
-                using (var cmdupdate2 = new MySqlCommand(MySqlCommandType.UPDATE))
-                    cmdupdate2.Update("entities").Set("name", names.NewName).Set("namechange", "")
+            foreach (var names in update.Values) {
+                using (var sqlCommand = new MySqlCommand(MySqlCommandType.UPDATE))
+                    sqlCommand.Update("entities").Set("name", names.NewName).Set("namechange", "")
                         .Where("name", names.OldName).Execute();
                 Console.WriteLine(" -[" + names.OldName + "] : -[" + names.NewName + "]");
             }
 
-            UPDATE.Clear();
+            update.Clear();
         }
     }
 
     public class NameChangeC {
-        public String NewName;
-        public String OldName;
+        public required string NewName;
+        public required string OldName;
     }
 }
